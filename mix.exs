745a5dd2bba10232f01cf004b398a2a438331142
Susumu Yamazaki <zacky1972@gmail.com>
2019-02-02 14:09:36 +0900

    add description and package

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Gulgan.MixProject do
       version: "0.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,6 +23,19 @@ defmodule Gulgan.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+    ]
+  end
+
+  defp description() do
+    "Gulgan"
+  end
+
+  defp package() do
+    [
+      name: "galgan",
+      maintainers: ["Kenta Tanaka", "Susumu Yamazaki", "Masakazu Mori", "Yuki Hisae"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/zeam-vm/gulgan"}
     ]
   end
 end
